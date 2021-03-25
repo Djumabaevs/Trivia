1a105e62b193e04ceb7e270fa45977d1d3f7b29a
Bakyt <djumabaevb@gmail.com>
2021-03-25 11:53:25 +0600

    created json array request

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.trivia.controller.AppController;
+
 public class MainActivity extends AppCompatActivity {
 
     String url = "http://raw.githubusercontent.com/curiousily/simple-quiz/master/script/statements-data.json";
@@ -11,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                response -> {
+
+                }, error -> {
+
+        });
+
+        AppController.getInstance().addToRequestQueue(jsonArrayRequest);
+
     }
 }
