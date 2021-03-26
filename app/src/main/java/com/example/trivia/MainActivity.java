aeedbc368ef3b149663d691c4f4b18a7c8a3b238
Bakyt <djumabaevb@gmail.com>
2021-03-26 17:08:18 +0600

    first animation

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -5,20 +5,25 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.example.trivia.data.AnswerListAsuncResponse;
 import com.example.trivia.data.AnswerListAsyncResponse;
 import com.example.trivia.data.Repository;
 import com.example.trivia.databinding.ActivityMainBinding;
 import com.example.trivia.model.Question;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     private ActivityMainBinding binding;
     private int currentQuestionIndex = 0;
+    List<Question> questions;
 
 
     @Override
@@ -30,12 +35,58 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        List<Question> questions = new Repository().getQuestions(questionArrayList ->
+        List<Question> questionsFirst = new Repository().getQuestions(questionArrayList ->
                 Log.d("Main", "onCreate: " + questionArrayList));
-        new Repository().getQuestions(listOfQuestions ->
-                binding.questionTextView.setText(listOfQuestions.get(currentQuestionIndex).getAnswer()));
+        questions = new Repository().getQuestions(listOfQuestions -> {
 
+                binding.questionTextView.setText(listOfQuestions.get(currentQuestionIndex).getAnswer());
 
+                updateCounter(listOfQuestions);
+        });
+
+        binding.buttonNext.setOnClickListener(view -> {
+            currentQuestionIndex = (currentQuestionIndex  +1) % questions.size();
+            updateQuestion();
+        });
+        binding.buttonTrue.setOnClickListener(view -> {
+
+            checkAnswer(true);
+
+        });
+        binding.buttonFalse.setOnClickListener(view -> {
+
+            checkAnswer(false);
+            shakeAnimation();
+
+        });
 
     }
+
+    private void checkAnswer(boolean userChooseCorrect) {
+        boolean answer = questions.get(currentQuestionIndex).getAnswerTrue();
+        int snackMessageId = 0;
+        if(userChooseCorrect == answer) {
+            snackMessageId = R.string.correct_answer;
+        } else {
+            snackMessageId  =R.string.incorrect_answer;
+        }
+        Snackbar.make(binding.cardView, snackMessageId, Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void updateCounter(ArrayList<Question> arrayList) {
+        binding.textViewOutOf.setText(String.format(getString(R.string.textQuestion),
+                currentQuestionIndex, questions.size()));
+    }
+
+    private void updateQuestion() {
+        String question = questions.get(currentQuestionIndex).getAnswer();
+        binding.questionTextView.setText(question);
+        updateCounter((ArrayList<Question>) questions);
+    }
+    private void shakeAnimation() {
+        Animation shake = AnimationUtils.loadAnimation(MainActivity.this,
+                R.anim.shake_animation);
+        binding.cardView.setAnimation(shake);
+    }
+
 }
